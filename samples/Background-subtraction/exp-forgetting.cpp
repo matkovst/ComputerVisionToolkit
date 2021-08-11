5d64dbbe51b8af098cb28b1d6ad45380a1cb07fd
Stanislav Matkov <st.matkov@yandex.ru>
2021-08-11 21:51:42 +0300

    BG-subtraction: Replace absdiff with Mahalanobis distance

--- samples/Background-subtraction/exp-forgetting.cpp
+++ samples/Background-subtraction/exp-forgetting.cpp
@@ -36,7 +36,6 @@ public:
         : m_alpha(alpha)
     {
     }
-
     
     /** @brief default destructor
     */
@@ -44,9 +43,11 @@ public:
 
     /** @brief Computes a foreground mask.
      */
-    CV_WRAP virtual void apply(cv::InputArray _image, cv::OutputArray _fgMask, double learningRate=-1) CV_OVERRIDE 
+    CV_WRAP virtual void apply(cv::InputArray _image, cv::OutputArray _fgMask, double learningRate = -1) CV_OVERRIDE 
     {
         std::ignore = learningRate;
+
+        ++m_frameNum;
 
         cv::Mat image = _image.getMat();
         cv::Mat image_32FC3;
@@ -54,23 +55,50 @@ public:
         cv::Mat fgMask = _fgMask.getMat();
         cv::Mat fgMask_32FC1;
 
-        if ( m_background.empty() )
+        if ( m_background_32FC3.empty() )
         {
-            image.convertTo(m_background, CV_32FC3);
+            image.convertTo(m_background_32FC3, CV_32FC3);
+            m_stdev2 = cv::Mat::zeros(m_background_32FC3.size(), m_background_32FC3.type());
+            m_MahalanobisDist = cv::Mat::zeros(m_background_32FC3.size(), CV_32FC1);
+            return;
         }
-        else
-        {
-            image.convertTo(image_32FC3, CV_32FC3);
-            m_background = (1 - m_alpha) * m_background + m_alpha * image_32FC3;
+        
+        /* Pre-process */
+        image.convertTo(image_32FC3, CV_32FC3);
+        cv::subtract(image_32FC3, m_background_32FC3, m_prevMeanDiff);
 
-            cv::Mat diff_32FC3;
-            cv::absdiff(image_32FC3, m_background, diff_32FC3);
-            cv::cvtColor(diff_32FC3, fgMask_32FC1, cv::COLOR_BGR2GRAY);
-            fgMask_32FC1.convertTo(fgMask, CV_8UC1);
-        }
+        /* Compute exp forgetting mean */
+        cv::accumulateWeighted(image_32FC3, m_background_32FC3, m_alpha);
+
+        /* Compute exp forgetting variance */
+        cv::subtract(image_32FC3, m_background_32FC3, m_currMeanDiff);
+        cv::pow(m_currMeanDiff, 2, m_currMeanDiff2);
+        cv::accumulateWeighted(m_currMeanDiff2, m_stdev2, m_alpha);
+
+        // /* Compute variance via Welford's online algorithm */
+        // if ( m_M2n.empty() ) 
+        // {
+        //     m_M2n = cv::Mat::zeros(image_32FC3.size(), image_32FC3.type());
+        // }
+        // cv::accumulateProduct(m_prevMeanDiff, m_currMeanDiff, m_M2n);
+        // m_stdev2 = m_M2n / m_frameNum;
+
+        /* Compute Mahalanobis distance btw Image and Background */
+        cv::divide(m_currMeanDiff2, m_stdev2, m_MahalanobisDistVec);
+        cv::transform(m_MahalanobisDistVec, m_MahalanobisDist, cv::Matx13f(1, 1, 1));
+        cv::sqrt(m_MahalanobisDist, m_MahalanobisDist);
+
+        // /* Segment fg/bg with absdiff */
+        // cv::Mat diff_32FC3;
+        // cv::absdiff(image_32FC3, m_background_32FC3, diff_32FC3);
+        // cv::cvtColor(diff_32FC3, fgMask_32FC1, cv::COLOR_BGR2GRAY);
+        // fgMask_32FC1.convertTo(fgMask, CV_8UC1);
+
+        /* Segment fg/bg with Mahalanobis dist */
+        m_MahalanobisDist.convertTo(fgMask, CV_8UC1);
     }
 
-    /** @brief Computes a background image.
+    /** @brief Returns a background image in CV_8UC3 type.
 
     @param backgroundImage The output background image.
 
@@ -79,7 +107,25 @@ public:
      */
     CV_WRAP virtual void getBackgroundImage(cv::OutputArray _backgroundImage) const 
     {
-        m_background.convertTo(_backgroundImage, CV_8UC3);
+        m_background_32FC3.convertTo(_backgroundImage, CV_8UC3);
+    }
+
+    /** @brief Returns a stdev2 image in CV_8UC3 type.
+
+    @param backgroundImage The output stdev2 image.
+     */
+    CV_WRAP virtual void getStdev2Image(cv::OutputArray _stdev2Image) const 
+    {
+        m_stdev2.convertTo(_stdev2Image, CV_8UC3);
+    }
+
+    /** @brief Returns a mahalanobis distance image in CV_8UC3 type.
+
+    @param backgroundImage The output stdev2 image.
+     */
+    CV_WRAP virtual void getMahalanobisDistImage(cv::OutputArray _mahalanobisDistImage) const 
+    {
+        m_MahalanobisDist.convertTo(_mahalanobisDistImage, CV_8UC3);
     }
 
     /** @brief Returns the alpha
@@ -96,8 +142,16 @@ public:
     }
 
 private:
+    unsigned long long m_frameNum { 0 };
     float m_alpha { 0.02f };
-    cv::Mat m_background;
+    cv::Mat m_background_32FC3;
+    cv::Mat m_prevMeanDiff;
+    cv::Mat m_currMeanDiff;
+    cv::Mat m_currMeanDiff2;
+    cv::Mat m_M2n;
+    cv::Mat m_stdev2;
+    cv::Mat m_MahalanobisDistVec;
+    cv::Mat m_MahalanobisDist;
 };
 
 /** @brief Creates EF Background Subtractor
@@ -162,6 +216,7 @@ int main(int argc, char** argv)
     cv::Mat frame, frame_32FC3, out;
     cv::Mat background_32FC3, background_8UC3;
     cv::Mat fgMask = cv::Mat::zeros(player->frame0().size(), player->frame0().type());
+    cv::Mat blank = cv::Mat::zeros(player->frame0().size(), player->frame0().type());
     while ( loop )
     {
         /* Controls */
@@ -196,7 +251,10 @@ int main(int argc, char** argv)
         {
             cv::cvtColor(fgMask, fgMask, cv::COLOR_GRAY2BGR);
         }
-        cvt::hstack2images( fgMask, frame, out );
+        cv::Mat stdev2;
+        bgSubtractor->getStdev2Image(stdev2);
+        // cvt::hstack2images( fgMask, frame, out );
+        cvt::stack4images( fgMask, frame, stdev2, blank, out );
         if ( record )
         {
             *player << out;
