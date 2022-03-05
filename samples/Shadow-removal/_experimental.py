96c95559977a7e82c126af9584f0c1028afd8e3a
Stanislav Matkov <st.matkov@yandex.ru>
2022-03-05 20:35:33 +0300

    [Shadow-removal] Draw fantastic rotated Gaussian

--- samples/Shadow-removal/_experimental.py
+++ samples/Shadow-removal/_experimental.py
@@ -1,8 +1,8 @@
+import math
+import argparse
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-import argparse
-import math
 
 U = np.array([ [1/math.sqrt(2), -1/math.sqrt(2), 0], [1/math.sqrt(6), 1/math.sqrt(6), -2/math.sqrt(6)] ]) # orthogonal matrix
 
@@ -64,6 +64,37 @@ def solveProjection(img, Rho, nangles = 181):
     minEntropyAngle = radians[minEntropyIdx]
     return minEntropy, minEntropyAngle, Entropies
 
+def L1(img, Rho, minEntropyAngle):
+    N = img.shape[0] * img.shape[1]
+    Chi = projectOntoPlane(Rho, U)
+    e = np.array([-1 * math.sin(minEntropyAngle), math.cos(minEntropyAngle)])
+    eT = np.array([np.cos(minEntropyAngle), np.sin(minEntropyAngle)])
+    Ptheta = np.outer(eT, eT)
+    Chitheta = Chi @ Ptheta.T
+    I = Chi @ e
+    Itheta = Chitheta @ e
+
+    IMostBrightest = np.sort( I.reshape(I.shape[0] * I.shape[1]) )
+    IMostBrightest = IMostBrightest[IMostBrightest.size - int(0.01*math.ceil(N)) : IMostBrightest.size]
+    IMostBrightestTheta = np.sort( Itheta.reshape(Itheta.shape[0] * Itheta.shape[1]) )
+    IMostBrightestTheta = IMostBrightestTheta[IMostBrightestTheta.size - int(0.01*math.ceil(N)) : IMostBrightestTheta.size]
+    ChiExtralight = (np.median(IMostBrightest) - np.median(IMostBrightestTheta)) * e
+    Chitheta += ChiExtralight
+
+    Rhoti = Chitheta @ U
+    cti = np.exp(Rhoti)
+    ctiSum = np.sum(cti, axis = 2)
+    ctiSum = ctiSum.reshape(cti.shape[0], cti.shape[1], 1)
+    rti = cti / ctiSum
+
+    # cv.imshow("I1D", cv.normalize(rti, 0, 255, cv.NORM_MINMAX))
+    # cv.waitKey()
+
+    # print(ChiExtralight)
+    # exit();
+    
+
+
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
@@ -78,8 +109,10 @@ if __name__ == "__main__":
     # Infer intrinsic image
     Rho = convertToLogChromacitySpace(img64)
     minEntropy, minEntropyAngle, Entropies = solveProjection(img, Rho, nangles = 181)
-    I1DAngle = minEntropyAngle + np.pi / 2 # I1DAngle = (158.0 * np.pi / 180.0)
-    I1D = getProjectedImage(Rho, I1DAngle)
+    invariantAngle = minEntropyAngle + np.pi / 2 # invariantAngle = (158.0 * np.pi / 180.0)
+    I1D = getProjectedImage(Rho, invariantAngle)
+
+    L1(img64, Rho, minEntropyAngle)
 
     # Display results
 
@@ -89,8 +122,41 @@ if __name__ == "__main__":
     Chi = projectOntoPlane(Rho, U)
     minX1, maxX1 = np.min(Chi[:,:,0]), np.max(Chi[:,:,0])
     minX2, maxX2 = np.min(Chi[:,:,1]), np.max(Chi[:,:,1])
-    lightnintDirection = [np.linspace(minX1, maxX1, 2) * np.cos(minEntropyAngle), np.linspace(minX1, maxX1, 2) * np.sin(minEntropyAngle)]
-    invatiantDirection = [np.linspace(minX2, maxX2, 2) * np.cos(minEntropyAngle + np.pi / 2), np.linspace(minX2, maxX2, 2) * np.sin(minEntropyAngle + np.pi / 2)]
+    chi1Range = maxX1 - minX1
+    chi2Range = maxX2 - minX2
+    chiCenter = np.array([(maxX1 + minX1) / 2.0, (maxX2 + minX2) / 2.0])
+    chiHypot = np.hypot(chi1Range, chi2Range)
+
+    RotMatx = lambda rad : np.array([[np.cos(rad), -np.sin(rad)], [np.sin(rad), np.cos(rad)]], dtype = np.float32)
+    def Shift(arrayXY, x, y):
+        shiftX, shiftY = np.mean(arrayXY[0]) - x, np.mean(arrayXY[1]) - y
+        arrayXY[0] -= shiftX
+        arrayXY[1] -= shiftY
+
+    lightnintDirection = np.array([[0, chiHypot], [0, 0]])
+    lightnintDirection = RotMatx(minEntropyAngle) @ lightnintDirection
+    Shift(lightnintDirection, chiCenter[0], chiCenter[1])
+    
+    invatiantDirection = np.array([[0, chiHypot], [0, 0]])
+    invatiantDirection = RotMatx(invariantAngle) @ invatiantDirection
+    Shift(invatiantDirection, maxX1, maxX2)
+
+    # Rotated Log Chromacity Gaussian
+    def GaussianPDF(x, mu, var):
+        denom = (2 * np.pi * var)**.5
+        num = np.exp(-(x - mu)**2 / (2 * var))
+        return num / denom
+    I = Chi[:,:,0] * np.cos(minEntropyAngle) + Chi[:,:,1] * np.sin(minEntropyAngle)
+    IMean = np.mean(I)
+    IStd = np.std(I)
+    lbound = IMean + 3.0 * (-IStd)
+    rbound = IMean + 3.0 * (+IStd)
+    GaussXAxis = np.linspace(lbound, rbound, 100)
+    GaussYAxis = GaussianPDF(GaussXAxis, IMean, IStd**2)
+    GaussYAxis /= np.linalg.norm(GaussYAxis)
+    ChiGauss = np.array([[GaussXAxis, GaussYAxis]]).squeeze(0)
+    ChiGauss = RotMatx(invariantAngle + np.pi) @ ChiGauss
+    Shift(ChiGauss, 1.5 * maxX1, 1.5 * maxX2)
 
     # 2D Log Chromacities plot
     figChrom = plt.figure()
@@ -103,7 +169,8 @@ if __name__ == "__main__":
     axChrom.scatter(Chi[:,:,0], Chi[:,:,1], marker = '.', color = 'dodgerblue', label = '2D log chromacity point')
     axChrom.plot(lightnintDirection[0], lightnintDirection[1], 'r--', label = 'Lightning direction')
     axChrom.plot(invatiantDirection[0], invatiantDirection[1], 'r-', label = 'Invariant direction')
-    axChrom.legend()
+    axChrom.plot(ChiGauss[0], ChiGauss[1], color = 'royalblue', label = 'Chi histogram distribution')
+    axChrom.legend(bbox_to_anchor=(0.9, 1.1))
 
     # Entropies plot
     figEntropy = plt.figure()
